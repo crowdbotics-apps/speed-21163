d41729319277b8d3e318274517393d5f32ca8abf
Crowdbotics <team@crowdbotics.com>
2020-10-06 15:09:44 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Feed3129446', human_name: 'Feed 3', access_route: 'Feed3129446'},
 { name: 'Feed5129445', human_name: 'Feed 5', access_route: 'Feed5129445'},
 { name: 'Feed6129444', human_name: 'Feed 6', access_route: 'Feed6129444'},
 { name: 'BlankScreen1129443', human_name: 'Blank Screen 1', access_route: 'BlankScreen1129443'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Feed3129446Navigator from '../features/Feed3129446/navigator';
 import Feed5129445Navigator from '../features/Feed5129445/navigator';
 import Feed6129444Navigator from '../features/Feed6129444/navigator';
 import BlankScreen1129443Navigator from '../features/BlankScreen1129443/navigator';
@@ -17,6 +18,7 @@ import BlankScreen0129442Navigator from '../features/BlankScreen0129442/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Feed3129446: { screen: Feed3129446Navigator },
 Feed5129445: { screen: Feed5129445Navigator },
 Feed6129444: { screen: Feed6129444Navigator },
 BlankScreen1129443: { screen: BlankScreen1129443Navigator },
